6fb4e2cf1355a52e5d0e01df7f5b5653d78158b8
Ragel Listiyono <143491291+ragellistiyono@users.noreply.github.com>
2023-09-24 01:23:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,25 +1,71 @@
-// Create Web Server
-// npm install express
-// npm install body-parser
-// npm install express-session
-// npm install express-validator
-// npm install multer
-// npm install mysql
-// npm install jade
-// npm install connect-flash
-// npm install passport
-// npm install passport-local
-// npm install passport-facebook
-// npm install passport-twitter
-// npm install passport-google-oauth
-// npm install nodemailer
-// npm install async
-// npm install node-schedule
-// npm install socket.io
-// npm install mongoose
-// npm install mongodb
-// npm install connect-mongo
-// npm install connect-flash
-// npm install express-error-handler
-// npm install cookie-parser
-// npm install express
+// Create web server
+var express = require('express');
+var router  = express.Router();
+
+// Import model
+var Comment = require('../models/comment');
+
+// Get all comments
+router.get('/', function(req, res) {
+    Comment.find(function(err, comments) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(comments);
+    });
+});
+
+// Get one comment
+router.get('/:id', function(req, res) {
+    Comment.findById(req.params.id, function(err, comment) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(comment);
+    });
+});
+
+// Create comment
+router.post('/', function(req, res) {
+    var comment = new Comment();
+    comment.name = req.body.name;
+    comment.text = req.body.text;
+    comment.save(function(err) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'Comment created!' });
+    });
+});
+
+// Update comment
+router.put('/:id', function(req, res) {
+    Comment.findById(req.params.id, function(err, comment) {
+        if (err) {
+            res.send(err);
+        }
+        comment.name = req.body.name;
+        comment.text = req.body.text;
+        comment.save(function(err) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({ message: 'Comment updated!' });
+        });
+    });
+});
+
+// Delete comment
+router.delete('/:id', function(req, res) {
+    Comment.remove({
+        _id: req.params.id
+    }, function(err, comment) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'Successfully deleted' });
+    });
+});
+
+// Export router
+module.exports = router;
